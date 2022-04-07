ec18d4aef21a506a275753c335064892fa9f0e37
JunYoung Lee <82147229+soft-jun@users.noreply.github.com>
2022-04-07 11:37:02 +0900

    Add files via upload

---
+++ test9.py
@@ -0,0 +1,13 @@
+from turtle import color, speed
+
+
+class Car:
+    def_init_(self, color, speed = 0):
+    self.cloor = color
+    self.speed = speed
+
+car = Car('black',0)
+car2 = Car('red',120)
+car3 = Car('yellow',30)
+car4 = Car('blue',0)
+car5 = Car('green',0)
