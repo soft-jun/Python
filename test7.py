32918269f79634eef31927e0820000d375cd5170
JunYoung Lee <82147229+soft-jun@users.noreply.github.com>
2022-04-07 11:36:33 +0900

    Add files via upload

---
+++ test7.py
@@ -0,0 +1,13 @@
+def calc_perimeter(radius) :
+    global perimeter
+    print("파이 값: ", pi)
+    perimeter = 2*pi * radius
+
+pi = 3.14159
+perimeter = 0
+calc_perimeter(10)
+print("원둘레(r=10) = ", perimeter) 
+
+
+
+
