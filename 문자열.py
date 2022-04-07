e12c298759c867b1524d788a4fcb5f5bba3fff7d
JunYoung Lee <82147229+soft-jun@users.noreply.github.com>
2022-04-07 11:37:40 +0900

    Add files via upload

---
+++ 문자열.py
@@ -0,0 +1,15 @@
+#turtle.shape('turtle')
+
+import turtle
+import math
+
+turtle.shape('turtle')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(140)
+
+turtle.forward(math.sqrt(2)*100)
+
+turtle.mainloop()
