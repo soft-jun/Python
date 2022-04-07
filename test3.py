8e1705e64ef5b675e4d7cd5d9146b37d1f1f49a0
JunYoung Lee <82147229+soft-jun@users.noreply.github.com>
2022-04-07 11:35:32 +0900

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,21 @@
+import rnadom
+
+num1 = []
+num.append(24)
+num.append(34)
+num.append(24)
+num.append(24)
+num.append(12)
+print(num1)
+
+num2 = []
+num2.append(100)
+num2.append(104)
+num2.append(103)
+num2.append(107)
+print(num2)
+
+num1.extend(num2)
+
+
+
